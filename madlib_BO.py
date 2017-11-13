21df0944ad4291f958b36c494134cdb9dd8e64b7
BenO11 <33633090+BenO11@users.noreply.github.com>
2017-11-13 11:47:52 -0500

    First upload

---
+++ madlib_BO.py
@@ -0,0 +1,72 @@
+### Mad Libs ###
+### Source: https://i.pinimg.com/736x/f7/7b/49/f77b492883b5eba4ad64b3ed02a51964--mad-lips-an-adjective.jpg
+### Ask Questions Here ###
+print("Write a noun")
+noun1 = input()
+
+print("Write a person in the room")
+room1 = input()
+
+print("Write a verb")
+verb1 = input()
+
+print("Write a part of the body (plural)")
+bodypart1 = input()
+
+print("Write an adjective")
+adj1 = input()
+
+print("Write a noun")
+noun2 = input()
+
+print("Write a noun")
+noun3 = input()
+
+print("Write a plural noun")
+pluralnoun1 = input()
+
+print("Write a type of liquid")
+liquid1 = input()
+
+print("Write an adjective")
+adj2 = input()
+
+print("Write a noun")
+noun4 = input()
+
+print("Write a noun")
+noun5 = input()
+
+print("Write a noun")
+noun6 = input()
+
+print("Write a plural noun")
+pluralnoun2 = input()
+
+print("Write a person in the room (female)")
+room2 = input()
+
+print("Write a noun")
+noun7 = input()
+
+print("Write a part of the body (plural)")
+bodypart2 = input()
+
+
+
+
+
+
+### Tell the MadLib ###
+print("It was Thanksgiving, and the scent of succulent roast " + noun1 + " wafted through my house.")
+print(room1 + " it's time to " + verb1 + "! my mother called.")
+print("I couldn't wait to get my " + bodypart1 + " on that " + adj1 + " Thanksgiving meal.")
+print("My family sat around the dinning-room " + noun2 +".")
+print("The table was laid out with every kind of" + noun3 +" imaginable.")
+print("There was a basket of hut buttered " + pluralnoun1 +"and glasses of sparkling " + liquid1 +".")
+print("The " + adj2 +"turkey sat, steaming, next to a tureen of " + noun4 +"gravy.")
+print("A bowl of ruby-red " + noun5 +" sauce, a sweet-" + noun6 +" casserole, and a dish of mashed " + pluralnoun2 +"tempted my taste buds.")
+print("But the dish I looked forard to most was Granma " + room2 +"'s famous " + noun7 +"pie.")
+print("Thanksgiving is my favorite holiday, " + bodypart2 +"down.")
+
+
---
+++ personality_survey_BO.py
@@ -0,0 +1,31 @@
+print("Whats's your favorite sport")
+sport = input().lower()
+if sport == "hockey":
+      print("That's my favorite too!")
+elif sport == "ping-pong":
+      print("I want to go to the pros too.")
+elif sport == "lacrosse":
+      print("Same")
+
+else:
+      print(sport + " sounds fun.")
+
+print("What's your favorite TV show?")
+show = input ()
+
+if show == "The League":
+    print("That's litterally the funniest show")
+elif show == "The Flash":
+    print("I love DC to")
+else:
+    print
+    (show + " Sounds good!")
+
+print("What's your favorite video game?")
+videgame = input().lower()
+if videogame == "NHL 18":
+    print("That game is so much fun to play!")
+elif videogame == "Fifa":
+    print("That game sounds like a lot of fun")
+else:
+    print(videogame + " sounds fun.")
